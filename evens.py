96eb0b08f0648dd83b4bc93ab6b9556731be700e
SDDNG <davidangraham@gmail.com>
2022-06-09 15:27:00 +0000

    test student

--- evens.py
+++ evens.py
@@ -18,4 +18,4 @@ def even_number_of_evens(numbers):
         raise TypeError("A list was not passed into the function")
 
 if __name__ == "__main__":
-    print(even_number_of_evens(5))
+    print(even_number_of_evens([5, 2, 4, 6]))
